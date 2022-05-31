802bd278953cb5fa1d2aa98fb62a9e4a8f0f9a41
Apvivar92@gmail.com <Apvivar92@gmail.com>
2022-05-30 17:14:56 -0700

    completed button state value is being tracked as well as able to toggle values true and false

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -5,10 +5,29 @@ const Task = ({ text, task, tasks, setTasks }) => {
   const deleteEvent = () => {
     setTasks(tasks.filter((element) => element.id !== task.id));
   };
+
+  const completeEvent = () => {
+    setTasks(
+      tasks.map((el) => {
+        //add if statement to change state value from falst to true
+        if (el.id === task.id) {
+          return {
+            ...el,
+            completed: !el.completed,
+          };
+        }
+        return el;
+      })
+    );
+  };
+
   return (
     <div className='task'>
-      <li className='task-item'>{text}</li>
-      <button>
+      {/* ternary operator to toggle value between true and false */}
+      <li className={`task-item ${task.completed ? 'completed' : ''}`}>
+        {text}
+      </li>
+      <button onClick={completeEvent}>
         <i>Complete</i>
       </button>
       <button onClick={deleteEvent}>
