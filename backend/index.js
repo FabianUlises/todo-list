741dcc60a998c7a80e00cd25ac25e103f54cd587
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-01 19:07:51 -0700

    Started working on server

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require('express');
+// Configuration
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Todo List');
+});
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>');
+});
+
+// Server Litening
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+});
