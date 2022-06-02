8f58279b4d4884e2ffe1b5949ba4655298addf93
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-02 16:12:01 -0700

    Added delete route to server

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,18 @@ app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
 });
-app.post('/todos/new', async (req, res) => {
+app.post('/todo/new', async (req, res) => {
     const todo = new Todo({
         text: req.body.text
     });
     todo.save();
     res.json(todo);
 });
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+})
 
 
 app.get('*', (req, res) => {
