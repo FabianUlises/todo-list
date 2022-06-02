200b52e1cd4e1d6c8a529b3c492fb5c8dd923d3e
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-02 15:58:47 -0700

    App connected to server. Also added post route to server file

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,24 @@ const connection = require('./tododb');
 const PORT = process.env.PORT || 3000;
 
 connection();
-
+const Todo = require('./models/Todo');
 // Routes
 app.get('/', (req, res) => {
     res.send('Todo List');
 });
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+app.post('/todos/new', async (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+});
+
+
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>');
 });
