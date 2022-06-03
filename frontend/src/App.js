9449b8841af341c55f55ff46c60734ef69aef0a7
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-02 18:57:48 -0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,79 @@
-import React, { useState } from 'react';
-import Input from './components/Input';
-import Filter from './components/Filter';
-import TaskList from './components/TaskList';
+import React, { useState, useEffect } from 'react';
+// import Input from './components/Input';
+// import Filter from './components/Filter';
+// import TaskList from './components/TaskList';
+
+
+const API = 'http://localhost:3000';
 
 function App() {
-  const [inputText, setInputText] = useState(''); //useState to track changes within input box
-  const [tasks, setTasks] = useState([]);
+  // State
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  // useEffects
+  useEffect(() => {
+    GetTodos();
+    console.log(todos)
+  }, [])
+
+  const GetTodos = () => {
+    fetch(API + "/todos")
+      .then(res => res.json())
+      .then(data => setTodos(data))
+      .catch(err => console.error('Error: ', err));
+  };
+
+  // const [inputText, setInputText] = useState(''); //useState to track changes within input box
+  // const [tasks, setTasks] = useState([]);
 
   return (
-    <div className='App'>
-      <header>ToDo App!</header>
-      <Input
-        inputText={inputText}
-        tasks={tasks}
-        setTasks={setTasks}
-        setInputText={setInputText}
-      />
-      <Filter />
-      <TaskList setTasks={setTasks} tasks={tasks} />
+    <div className="container">
+      <h1>Your Tasks</h1>
+
+      <div className="todos-container">
+        {todos.map(todo => (
+          <div className="todo complete">
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div className="delete-todo">x</div>
+          </div>
+        ))}
+      </div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div className='App'>
+    //   <header>ToDo App!</header>
+    //   <h3>Your Tasks</h3>
+    //   <Input
+    //     inputText={inputText}
+    //     tasks={tasks}
+    //     setTasks={setTasks}
+    //     setInputText={setInputText}
+    //   />
+    //   <Filter />
+    //   <TaskList setTasks={setTasks} tasks={tasks} />
+    // </div>
   );
 }
 
