a3a38f196d4c83615d81cbdde42545c9ac1d1787
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-04 12:49:47 -0700

    Connecting database to front end using fetch call.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,42 @@ import React, { useState, useEffect } from 'react';
 import Input from './components/Input';
 import Filter from './components/Filter';
 
+
+//  Connecting to database
+const API = 'http://localhost:3001';
+
+
 function App() {
+  // State
   const [inputText, setInputText] = useState(''); //useState to track changes and render within input box
   const [tasks, setTasks] = useState([]);
+
+  // useEffects
+  // Calling for api at page load
+  useEffect(() => {
+    // Function to get database
+    getTodos();
+  }, [])
+
+
+
+
+  // Functions
+  // Function to fetch database
+  const getTodos = () => {
+    // Fetching data
+    fetch(API = "/todos")
+      .then(res => res.json())
+      .then(data => setTasks(data))
+      .catch(err => console.error('Error: ', err));
+  };
+
+  // Complete button
+  const completeTask = async (id) => {
+    console.log('complete task');
+  };
+
+
 
   return (
     <div className='App'>
