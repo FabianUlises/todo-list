cad95fb2d135850ab8424204645ce6028226ed8f
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-05 17:36:25 -0700

    Moved all controller handlers into todoscontroller file

--- backend/app.js
+++ backend/app.js
@@ -5,45 +5,17 @@ const express = require('express');
 // Configuration
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors())
 // Connecting to mongoose
 const connection = require('./tododb');
 connection();
-
-// Requiring database from models dir
-const Todo = require('./models/Todo');
-
-
 // Routes
-const todoController = require('./controllers/todosController');
-app.get('/', todoController.home);
-app.get('/todos', async (req, res) => {
-    const todos = await Todo.find();
-    res.json(todos);
-});
-app.post('/todo/new', async (req, res) => {
-    const todo = new Todo({
-        text: req.body.text
-    });
-    todo.save();
-    res.json(todo);
-});
-app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
-
-    res.json(result);
-})
-app.put('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
-
-    todo.complete = !todo.complete;
-    todo.save();
-    res.json(todo);
-});
-
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>');
-});
-
+// Importing todocontroller handlers
+const todosController = require('./controllers/todosController');
+app.get('/', todosController.home);
+app.get('/todos', todosController.getAllTodos);
+app.post('/todo/new', todosController.createTodo);
+app.delete('/todo/delete/:id',todosController.deleteTodo)
+app.put('/todo/complete/:id', todosController.updateTodo);
+app.get('*', todosController.catchAll);
 module.exports = app;
--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -1,3 +1,38 @@
+// Importing database from models dir
+const Todo = require('./../models/Todo');
+
+// Home controller
 exports.home = (req, res) => {
     res.status(200).send('Todo Lists');
-}
+};
+// Getall controller
+exports.getAllTodos = async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+};
+// Create controller
+exports.createTodo = async (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+};
+// Delete controller
+exports.deleteTodo = async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+};
+// Update controller
+exports.updateTodo = async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+};
+// Catch all controller
+exports.catchAll = (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>');
+};
