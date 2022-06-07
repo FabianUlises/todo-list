93deff0151c9a6020548fa9278ad258b9cbdcfa2
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-06 17:51:55 -0700

    Connected app to hosted database

--- backend/tododb.js
+++ backend/tododb.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
-
+const dbUrl = 'mongodb+srv://admin:secretPassword1!@cluster0.9mgft6t.mongodb.net/?retryWrites=true&w=majority';
 module.exports = async () => {
     try {
         const connectionParams = {
             useNewUrlParser: true,
             useUnifiedTopology: true
         };
-        await mongoose.connect(
-            'mongodb://127.0.0.1:27017/todo-list', connectionParams
-        );
+        await mongoose.connect(dbUrl, connectionParams);
         console.log('Connected to database')
     } catch (error) {
         console.log('Could not connect to database', error)
